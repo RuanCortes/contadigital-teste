16e7b4fc8c307611dd6eb99c459452f3bb86a168
Ruan <cortesruan@hotmail.com>
2023-09-10 19:31:57 -0300

    initial commit develop branch creation

---
+++ src/main/java/com/br/contadigitalteste/ContadigitalTesteApplication.java
@@ -0,0 +1,13 @@
+package com.br.contadigitalteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadigitalTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadigitalTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/contadigitalteste/ContadigitalTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.contadigitalteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadigitalTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
